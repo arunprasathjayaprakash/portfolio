cb9a72c578d1ca3fde73b1a8f8d98417704279c7
arunJP <arun826.jp@gmail.com>
2024-11-13 17:03:20 -0800

    Adding Updated code file

--- Scalable_model_pipelines/Models_As_Serverless_Functions/cloud_function_call.py
+++ Scalable_model_pipelines/Models_As_Serverless_Functions/cloud_function_call.py
@@ -1,13 +1,28 @@
+import google.oauth2.service_account as sc
+import google.auth
+from google.auth.transport.requests import Request
 import requests
+import json
 
-def call_cloudrun(url,default='Hello'):
+def call_cloudrun(url,default="Hello"):
 
-    #POST the url for a resonse
-    results = requests.post(url,
-                             json={'msg':default})
+    #Run Your GCP setup for authentication and add Cloud Run permissions as needed
+    credentials, _ = google.auth.default()
+    auth_request = Request()
+    credentials.refresh(auth_request)
+    id_token = credentials.id_token
 
-    return results.json()
+    payload = json.dumps({
+        "msg": "data"
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {id_token}'}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()
 
 if __name__ == "__main__":
-    url = 'https://us-central1-manifest-glyph-441000-g2.cloudfunctions.net/Cloud_trigger'
+    url = "https://cloud-trigger-32726136683.us-central1.run.app"
     call_cloudrun(url)
