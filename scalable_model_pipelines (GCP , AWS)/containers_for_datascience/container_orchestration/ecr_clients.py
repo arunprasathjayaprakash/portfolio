7b653a334e7497b71f0661650fe98f1d068edda5
arunJP <arun826.jp@gmail.com>
2024-12-09 16:54:33 -0800

    Added AWS ECR Docker scripts

--- scalable_model_pipelines (GCP , AWS)/containers_for_datascience/container_orchestration/ecr_clients.py
+++ scalable_model_pipelines (GCP , AWS)/containers_for_datascience/container_orchestration/ecr_clients.py
@@ -1,6 +1,7 @@
+import os
 import boto3
 
-def get_clients(service_type,region=None):
+def get_clients(service_type,region=None,verbose=True):
 
     try:
         if region != None:
@@ -24,20 +25,78 @@ def get_clients(service_type,region=None):
 
         if service_type == 'ecr':
             response = client_aws.describe_repositories()
+            repositories = []
 
             # Print repository details
+
             print("ECR Repositories:")
             for repo in response.get('repositories', []):
-                print(f"- Repository Name: {repo['repositoryName']}")
-                print(f"  URI: {repo['repositoryUri']}")
-                print(f"  Created At: {repo['createdAt']}")
-                print()
+                if verbose:
+                    print(f"- Repository Name: {repo['repositoryName']}")
+                    print(f"  URI: {repo['repositoryUri']}")
+                    print(f"  Created At: {repo['createdAt']}")
+                repositories.append({repo['repositoryName']: repo['repositoryUri']})
+
+            if repositories:
+                return client_aws , repositories
 
     except Exception as e:
         raise e
 
+def create_docker_image(image_tag,docker_config_path):
+
+    import docker
+
+    client = docker.from_env()
+
+    image , log = client.images.build(
+        path=docker_config_path,
+        tag=image_tag
+    )
+
+    for log in log:
+        if 'stream' in log:
+            print(log['stream'], end="")
+
+
+def push_docker_image(repo_url,image_tag):
+
+    import docker , base64
+
+    docker_client = docker.from_env()
+
+    ecr_client , repositories = get_clients(service_type,region="us-west-1",verbose=False)
+
+    ecr_credentials = (
+        ecr_client
+        .get_authorization_token()
+        ['authorizationData'][0])
+
+    ecr_username = 'AWS'
+
+    ecr_password = (
+        base64.b64decode(ecr_credentials['authorizationToken'])
+        .replace(b'AWS:', b'')
+        .decode('utf-8'))
+
+    ecr_url = ecr_credentials['proxyEndpoint']
+
+    # get Docker to login/authenticate with ECR
+    docker_client.login(
+        username=ecr_username, password=ecr_password, registry=ecr_url)
+
+    log = docker_client.images.push(
+        repository=repo_url,
+        tag=image_tag,
+        stream=True
+    )
+
+    for l in log:
+        print(l)
 
 if __name__ == "__main__":
     service_type = 'ecr'
-    get_clients(service_type,region="us-west-1")
-
+    client , repositories = get_clients(service_type,region="us-west-1")
+    repo_uri = repositories[0]['educative/containers']
+    create_docker_image(repo_uri+":1.0","./app")
+    push_docker_image(repo_uri,"1.0")
